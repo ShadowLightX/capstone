5826ae36cf2bfe46d91468b3a9948b478550eeff
Bob Doyle <lpnbd@swcp.com>
2014-09-04 17:22:32 -0600

    sign up

---
+++ signup.php
@@ -0,0 +1,31 @@
+<?php
+// define variables and set to empty values
+$firstName = "";
+$lastName = "";
+$userName = "";
+$password = "";
+$confirm = "";
+$email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = test_input($POST["firstName"]);
+    echo $firstName;
+    $lastName = test_input($POST["lastName"]);
+    echo $lastName;
+    $userName = test_input($POST["userName"]);
+    echo $userName;
+    $password = test_input($POST["password"]);
+    echo $password;
+    $confirm = test_input($POST["confirm"]);
+    echo $confirm;
+    $email = test_input($POST["email"]);
+    echo $email;
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
