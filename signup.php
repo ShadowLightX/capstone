49378bd85eae8f1c3695104a3de1ec7b11d6cfd4
Bob Doyle <lpnbd@swcp.com>
2014-09-06 14:57:41 -0600

    revert to 1st version

--- signup.php
+++ signup.php
@@ -1,66 +1,38 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <style>
-            .error {color: #FF0000;}
-        </style>
     </head>
     <body>
-        <?php
-        // Initialize variables and set to empy strings
-        $firstName = $lastName = "";
-        $firstNameErr = $lastNameErr = "";
-        
-        // Control variables
-        $app_state = "emtpy";  // empty, processed, logged in
-        $valid = 0;
-        
-        // Validate input and sanitize
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            if(empty($_POST["firstName"])) {
-                $firstNameErr = "First name is required";
-            }
-            else {
-                $firstName = test_input($_POST["firstName"]);
-                $valid++;
-            }
-            if (empty($_POST["lastName"])) {
-                $lastNameErr = "Last name is reuqired";
-            }
-            
-            if($valid >= 2) {
-                $app_state = "processed";
-            }
-        }
-        
-        //Sanitize data
-        function test_input($data) {
-            $data = trim(data);
-            $data = stripslashes(data);
-            $data = htmlspecialchars(data);
-            return $data;
-        }
-        
-        if ($app_state == "empty") {
-        ?>
-        <h2>Find User</h2>
-        <p><span class="error>* required</span></p>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-        First Name: <input type="text" name="firstName" value="<?php echo $firstName; ?>"><span class="error">* <?php echo $fisrtNameErr; ?></span><br><br>
-        Last Name: <input type="text" name="lastName" value="<?php echo $lastName; ?>"><span class="error">* <?php echo $lastNameErr; ?></span><br<br>
-        <input type="submit">
-        </form>
-    </body>
-</html>
 <?php
-}
-elseif($app_state == "processed") {
-    if($firstName == "Vincent") {
-        $app_state = "Logged in";
-    }
+// define variables and set to empty values
+$firstName = "";
+$lastName = "";
+$userName = "";
+$password = "";
+$confirm = "";
+$email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $_REQUEST["firstName"];
+    echo $firstName;
+    $lastName = $_REQUEST["lastName"];
+    echo $lastName;
+    $userName = $_REQUEST["userName"];
+    echo $userName;
+    $password = $_REQUEST["password"];
+    echo $password;
+    $confirm = $_REQUEST["confirm"];
+    echo $confirm;
+    $email = $_REQUEST["email"];
+    echo $email;
 }
 
-if ($app_state == "Logged in") {
-    echo("logged in<br> Hello Vincent</body></html>");
+function test_input($data) {
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 ?>
+    </body>
+</html>
