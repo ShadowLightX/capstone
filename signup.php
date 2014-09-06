efb7fa325856a49750aa9a9bef44bcdf86742a10
Bob Doyle <lpnbd@swcp.com>
2014-09-06 15:52:46 -0600

    refactor html form and php

--- signup.php
+++ signup.php
@@ -11,20 +11,49 @@ $userName = "";
 $password = "";
 $confirm = "";
 $email = "";
+$firstNameErr = "";
+$lastNameErr = "";
+$userNameErr = "";
+$passwordErr = "";
+$confirmErr = "";
+$emailErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = $_REQUEST["firstName"];
-    echo $firstName;
-    $lastName = $_REQUEST["lastName"];
-    echo $lastName;
-    $userName = $_REQUEST["userName"];
-    echo $userName;
-    $password = $_REQUEST["password"];
-    echo $password;
-    $confirm = $_REQUEST["confirm"];
-    echo $confirm;
-    $email = $_REQUEST["email"];
-    echo $email;
+    if(empty($_POST["firstName"])) {
+        $firstNameErr = "First name is required";
+    } else {
+        $firstName = test_input($_POST["firstName"]);
+    }
+    
+    if(empty($_POST["lastName"])) {
+        $lastNameErr = "Last name is required";
+    } else {
+        $lastName = test_input($_POST["lastName"]);
+    }
+    
+    if(empty($_POST["userName"])) {
+        $userNameErr = "User name is required";
+    } else {
+        $userName = test_input($_POST["userName"]);
+    }
+    
+    if(empty($_POST["password"])) {
+        $passwordNameErr = "Password is required";
+    } else {
+        $password = test_input($_POST["password"]);
+    }
+    
+    if(empty($_POST["confirm"])) {
+        $confirmErr = "Confirm password";
+    } else {
+        $confirm = test_input($_POST["confirm"]);
+    }
+    
+    if(empty($_POST["email"])) {
+        $email = "E-mail address required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
 }
 
 function test_input($data) {
