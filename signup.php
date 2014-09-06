b44c5042a04c928c7766e5d31eb9557e4db59ded
Bob Doyle <lpnbd@swcp.com>
2014-09-06 11:13:19 -0600

    refactored php

--- signup.php
+++ signup.php
@@ -1,41 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
 <?php
-// define variables and set to empty values
-$firstName = "";
-$lastName = "";
-$userName = "";
-$password = "";
-$confirm = "";
-$email = "";
+$firstName = check_input($_POST['firstName'], "Enter your first name");
+$lastName = check_input($_POST['lastName'], "Enter your last name");
+$userName = check_input($_POST['userName'], "Enter your user name");
+$password = check_input($_POST['password'], "Create your password");
+$confirm = check_input($_POST['confirm'], "Confirm your password");
+$email = check_input($_POST['email'], "Enter your e-mail");
+?>
+<html>
+    <body>
+        <p>Welcome <?php echo $userName; ?></p><br>
+    </body>
+</html>
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = test_input($_POST['firstName'],"Please enter your first name");
-    echo $firstName;
-    $lastName = test_input($_POST['lastName'],"Please enter your last name");
-    echo $lastName;
-    $userName = test_input($_POST['userName'],"Please enter your user name");
-    echo $userName;
-    $password = test_input($_POST['password'],"Please create your password");
-    echo $password;
-    $confirm = test_input($_POST['firstName'],"Please confirm your password");
-    echo $confirm;
-    $email = test_input($_POST['email'],"Please enter your e-mail");
-    echo $email;
-}
-
-function test_input($data, $problem='') {
+<?php
+function check_input($data, $problem='') {
     $data = trim($data);
-    $data = stripcslashes($data);
+    $data = stripslashes($data);
     $data = htmlspecialchars($data);
-    if ($problem && strlen($data) == 0) {
+    if (problem && strlen($data) == 0) {
         show_error($problem);
     }
     return $data;
 }
+
+function show_error($myError) {
+}
 ?>
-    </body>
-</html>
+    <html>
+        <body>
+            <strong>Please correct the following error:</strong><br>
+            <?php echo $myError; ?>
+<?php
+    exit();
+?>
