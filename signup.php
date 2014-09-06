89af2ff0f957751f2e213362210c4ae669962c00
Bob Doyle <lpnbd@swcp.com>
2014-09-06 09:46:16 -0600

    added required fields to form

--- signup.php
+++ signup.php
@@ -13,24 +13,27 @@ $confirm = "";
 $email = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = $_REQUEST["firstName"];
+    $firstName = test_input($POST['firstName'],"Please enter your first name");
     echo $firstName;
-    $lastName = $_REQUEST["lastName"];
+    $lastName = test_input($POST['lastName'],"Please enter your last name");
     echo $lastName;
-    $userName = $_REQUEST["userName"];
+    $userName = test_input($POST['userName'],"Please enter your user name");
     echo $userName;
-    $password = $_REQUEST["password"];
+    $password = test_input($POST['password'],"Please create your password");
     echo $password;
-    $confirm = $_REQUEST["confirm"];
+    $confirm = test_input($POST['firstName'],"Please confirm your password");
     echo $confirm;
-    $email = $_REQUEST["email"];
+    $email = test_input($POST['email'],"Please enter your e-mail");
     echo $email;
 }
 
-function test_input($data) {
+function test_input($data, $problem='') {
     $data = trim($data);
     $data = stripcslashes($data);
     $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0) {
+        show_error($problem);
+    }
     return $data;
 }
 ?>
