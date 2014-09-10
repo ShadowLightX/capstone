a7a452ac39b7ea404c63f475a4a93302e4aaaeb2
Bob Doyle <lpnbd@swcp.com>
2014-09-10 10:51:38 -0600

    refactored html and php

---
+++ javascript/jquery-form.js
@@ -0,0 +1,72 @@
+$(document).ready(
+    function() {
+    $("#signUpForm").validate({
+        //DEBUG MORE: stop the form submission...
+        // ...delete is after debugging
+        
+         // debug: true,
+         // errorclass: formatting of messages
+         errorClass: "badForm",
+         
+        // rules define how fields should be sanitized
+        rules: {
+            email: {
+                email: true,
+                required: true
+                },
+            password: {
+                minlength: 8
+                },
+                confirmPassword: {
+                    equalTo: "#password"
+                },
+                napHours: {
+                    // setting up a min enforces numeric (integer, double) types
+                    min: 0,
+                    required: true
+                }
+            },
+            
+            // messages define what we tell the user
+            messages: {
+                email: {
+                    // email is the message they get for input that's invalid
+                      email: "please enter a valid Email",
+                     // required is the message they get for omitting the Email
+                      required: "Please enter anything at all"
+                },
+                  // it is not necessary to expand when there's only one rule
+                  password: {
+                    minlength: "Please enter a password of at least 8 characters",
+                    required: "Please enter a password",
+                },
+                  confirmPassword: "Passwords do not match",
+                  napHours: {
+                    min: "Please enter 0 or more hours",
+                    required : "Please enter the number of nap hours"
+                }
+            },
+            
+            // setup the AJAX call
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    // How do we send this data? GET or POST
+                    type: "POST",
+                    // URL to submit to
+                    // SECURITY WARNING: browsers don't allow you to submit to
+                    // an external server - you can only load from your own
+                    url: "jquery-form.php",
+                    // data to submit
+                    data: $(form).serialize(),
+                    // anonymous callback function to react to a successful query
+                    success: function(ajaxOutput) {
+                        // html() is jQuery's .innerHTML
+                        $("#outputArea").html(ajaxOutput);
+                    }
+                    
+                });
+            }
+        });
+    }
+    
+);
--- signup.php
+++ signup.php
@@ -3,6 +3,21 @@
 // expect all the form values to be loaded into the $_POST superglobal automatically
 // (e.g., the email form value is in $_POST["email"])
 
+// define variables and set to empty values
+$firstName       = "";
+$lastName        = "";
+$userName        = "";
+$password        = "";
+$confirmPassword = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["firstName"])) {
+    $firstNameErr = "Name is required";
+  } else {
+    $firstName = test_input($_POST["firstName"]);
+  }
+
 // try the sanitization so we can format errors later on
 try {
     // if the Email has no @ character, throw an exception
@@ -27,25 +42,15 @@ try {
     }
     
     // passwords are safe & match
-    echo "Your password $safePassword is safe with me! <br />";
+    echo "Your password is confirmed. <br />";
     
-    // this code is VULNERABLE to a cross site scripting attack
-    // echo "Your password " . $_POST["password"] . " is safe with me!<br />";
-    
-    // verify the nap hours are a double
-    if(filter_input(INPUT_POST, "napHours", FILTER_VALIDATE_FLOAT) === false) {
-        throw(new Exception("Please enter the number of nap hours"));
-    }
-
-    // now sanitize the hours and convert them to a double
-    $safeNapHours = filter_input(INPUT_POST, "napHours", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $safeNapHours = floatval($safeNapHours);
-    
-    //nap hours converted
-    echo "Have a good nap for the next $safeNapHours hours";
 }
 // catch the exception and format it as an error message
 catch(Exception $error) {
     echo "<span class='badForm'>" . $error->getMessage() . "</span>";
+    }
 }
+    function test_input($input) {
+        return $input;
+    }
 ?>
