2c1a3ea910a6719579d374ec1c3d32df85fa49d5
charlie goodan <cgoodan@gmail.com>
2014-09-10 14:34:54 -0600

    drop down menus! https://app.asana.com/0/15833644813498/15853784456137

--- javascript/neutrality.js
+++ javascript/neutrality.js
@@ -1,1 +1,13 @@
-$('.dropdown-toggle').dropdown();
+$(document).ready(
+    function() {
+        $('.myMenu > li').mouseover(
+	    function(){
+		$(this).find('ul').css('visibility', 'visible');
+	    }
+	);
+        $('.myMenu > li').mouseout(
+	    function(){
+		$(this).find('ul').css('visibility', 'hidden');
+	    }
+	);  
+});
