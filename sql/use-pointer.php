5b025947c880248a5d075133db8b3ffac3c5e078
Dylan McDonald <dmcdonald21@cnm.edu>
2014-09-30 09:48:27 -0600

    Added proof of concept for using the singleton pattern https://app.asana.com/0/15843277689885/16117410053485

---
+++ sql/use-pointer.php
@@ -0,0 +1,11 @@
+<?php
+// require the shared mySQL class
+require_once("/etc/apache2/captstone-mysql/net-neutrality.php");
+
+// every time one needs mySQL access, all one needs to do is run a single method...
+try {
+    $mysqli = Pointer::getPointer();
+} catch (mysqli_sql_exception $sqlException) {
+    // handle connection errors here
+}
+?>
