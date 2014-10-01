4cb12ce487dd6c5e78c2d6631cae23149fc783cf
charlie goodan <cgoodan@gmail.com>
2014-10-01 13:57:09 -0600

    modularized article-template pages and removed thier html pages https://app.asana.com/0/15833644813498/15833644813498

---
+++ article-template1.php
@@ -0,0 +1,31 @@
+<?php
+require_once("header.html");
+?>
+	    <!--title-->
+	    <section id="title" class="col-xs-10 title text-center">
+		<h2>Title</h2>
+	    </section>
+	    <!--date-->
+	    <section id="date" class="col-xs-10 col-md-3 date text-center">
+		<h2>Date</h2>
+	    </section>
+	    <!--links-->
+	    <section id="links" class="col-xs-10 col-md-4 links text-center">
+		<h2>Links</h2>
+	    </section>
+	    <!--author-->
+	    <section id="author" class="col-xs-10 col-md-3 author text-center">
+		<h2>Author</h2>
+	    </section>
+	    <!--content-->
+	    <section id="content" class="col-xs-10 content">
+		<img class="img-responsive" src="images/...">
+		<h2>Content</h2>
+	    </section>
+	    <!--publisher-->
+	    <section id="publisher" class="col-xs-10 publisher text-center">
+		<h2>Publisher</h2>
+	    </section>
+<?php
+require_once("footer.html");
+?>
