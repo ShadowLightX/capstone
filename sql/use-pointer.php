394e49ca33e7a0f4e7e98f19a4139613a5054134
Modesto Ayala Ruth <tigrecientifico@gmail.com>
2014-10-01 10:56:14 -0600

    meddled with php code

--- sql/use-pointer.php
+++ sql/use-pointer.php
@@ -1,6 +1,6 @@
 <?php
 // require the shared mySQL class
-require_once("/etc/apache2/captstone-mysql/net-neutrality.php", "net_neutrality", "hatscrimwispycellscala", "net_neutrality");
+require_once("/etc/apache2/captstone-mysql/net-neutrality.php");
 
 // every time one needs mySQL access, all one needs to do is run a single method...
 try {
