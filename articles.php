ca8480ac49927343827d097ff0488ba4d7ab5640
charlie goodan <cgoodan@gmail.com>
2014-10-01 13:33:34 -0600

    modularized articles.html https://app.asana.com/0/15833644813498/15833644813498

---
+++ articles.php
@@ -0,0 +1,12 @@
+<?php
+require_once("header.html");
+?>
+	    <!--articles directory-->
+	    <ul>
+		<li class="text-center col-xs-10"><a href="article-template.html">Article</a></li>
+		<li class="text-center col-xs-10"><a href="articles-template1.html">Article1</a></li>
+		<li class="text-center col-xs-10"><a href="articles-template2.html">Article2</a></li>
+	    </ul>
+<?php
+require_once("footer.html");
+?>
