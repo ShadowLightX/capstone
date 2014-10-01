420749962e104a5668075281169fccb531623113
charlie goodan <cgoodan@gmail.com>
2014-10-01 13:48:45 -0600

    modularized videos.html, removed videod.html https://app.asana.com/0/15833644813498/15833644813498

---
+++ videos.php
@@ -0,0 +1,17 @@
+<?php
+require_once("header.html");
+?>
+	       <!--video-->
+	       </section>
+	       <!--start of form-->
+	       <h2 class="text-center"></h2><br><br><br>
+	       <div class="videoWrapper">
+		    <iframe src="//www.youtube.com/embed/NAxMyTwmu_M" frameborder="0" allowfullscreen></iframe>
+		    <div class="carousel-caption">
+			 Watch the video here!
+		    </div>
+	       </div>
+	       <!--<p id="outputArea"></p>-->
+<?php
+require_once("footer.html");
+?>
