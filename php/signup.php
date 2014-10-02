c2683ecfb007637aac0928bdbb60ce58061897c5
Nicholas Bowling <nickb505@msn.com>
2014-10-02 11:06:39 -0600

    changes https://app.asana.com/0/15843277689885/17094782348693

--- php/signup.php
+++ php/signup.php
@@ -1,124 +1,72 @@
-<!DOCTYPE html>
-<html>
-     <head>
-        <Title>Sign Up</title>
-        <meta charset="UTF-8" />
-            <!-- Format -->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <!-- Format -->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-            <!-- Stylesheet -->
-        <link rel="stylesheet" type="text/css" href="../css/neutrality.css" />
-            <!-- Bootstrap -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
-            <!-- Google font -->
-	<link href='http://fonts.googleapis.com/css?family=Geo' rel='stylesheet' type='text/css'>			
-	    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>  
-        <script src="//malsup.github.com/jquery.form.js"></script>
-        <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
-        <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/additional-methods.min.js"></script>
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="../javascript/bootstrap.min.js"></script>
-	<script src="../javascript/signup.js"></script>
-	<!-- link type="text/css" rel="stylesheet" href="css/jquery-form.css" /-->
-    </head>
-    <body>
-       <div id="container" class="container">
-	<div id="container" class="container col-xs-offset-1">
-	  <!--nav-->
-	  <ul class="row myMenu">
-	    <section id= "logo" class= "col-xs-10 col-md-2"><a href="index.html"><img src="../images/logo.png"></a></section>
-	    <li id= "home" class= "col-xs-10 col-md-2"><a href="index.html">Home</a></li>
-	    <li id= "resources" class="col-xs-10 col-md-2"><a href="resources.html">Resources</a>
-	      <ul>
-                <li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-		<li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-		<li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-		<li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-	      </ul>
-	    </li>
-	    <li id= "articles" class= "col-xs-10 col-md-2"><a href="articles.html">Articles</a>
-	      <ul>
-                <li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-                <li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-                <li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-                <li class= "col-xs-10 col-md-2"><a href="#">...</a></li>
-	      </ul>
-	    </li>
-		<li id= "forums" class= "col-xs-10 col-md-2"><a href="forums.html">Forum</a></li><br>
-		<li id= "search" class= "col-xs-10 col-md-4 reverse" ><a href="....html">Search</a></section>
-		<li id= "login" class= "col-xs-10 col-md-4 reverse"><a href="....html">Login</a></section>
-	    </ul>
-	
 <?php
 // notice the $_POST superglobal is an array!
 // expect all the form values to be loaded into the $_POST superglobal automatically
 // (e.g., the email form value is in $_POST["email"])
+// start the session and require the CSRF verifier
+session_start();
+require_once("cs.php");
+require_once("user.php");
+require_once("login.php");
 
-// define variables and set to empty values
-$firstName       = "";
-$lastName        = "";
-$userName        = "";
-$password        = "";
-$confirmPassword = "";
-// try the sanitization so we can format errors later on
-// filter firstName for bad stuff
+// verify the CSRF data
 try {
-    if(empty($_POST["firstName"])) {
-        $firstName = filter_input(INPUT_POST, "firstName", FILTER_SANITIZE_STRING);
-    }
+    $verified = verifyCsrf($_POST["csrfName"], $_POST["csrfToken"]);
+    if($verified === true) {
+	  $firstName       = null;
+	  $lastName        = null;
+	  $userName        = "";
+	  $password        = "";
+	  $confirmPassword = "";
+	  // try the sanitization so we can format errors later on
+	  // filter firstName for bad stuff
+	  try {
+	       if(!empty($_POST["firstName"])) {
+	            $firstName = filter_input(INPUT_POST, "firstName", FILTER_SANITIZE_STRING);
+	       }
+	  
+	       if(!empty($_POST["lastName"])) {
+	            $lastName = filter_input(INPUT_POST, "lastName", FILTER_SANITIZE_STRING);
+	       }
+	  
+	       if(empty($_POST["userName"])) {
+	            throw(new Exception("Please enter a User Name"));
+	       }
+	       else {
+	            $userName= filter_input(INPUT_POST, "userName", FILTER_SANITIZE_STRING);
+	       }
     
-    if(empty($_POST["lastName"])) {
-  
-        $lastName = filter_input(INPUT_POST, "lastName", FILTER_SANITIZE_STRING);
-    }
-    if(empty($_POST["userName"])) {
-        throw(new Exception("Please create your user name"));
-    } else {
-        $userName= filter_input(INPUT_POST, "userName", FILTER_SANITIZE_STRING);
-    }
+	       if(filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL) === false) {
+	            throw(new Exception("Please enter your Email address"));
+	       }
     
-    if(filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL) === false) {
-        throw(new Exception("Please enter your Email address"));
-    }
+	       // if filter_input passed the Email, we can use it
+	       $safeEmail = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
     
-    // if filter_input passed the Email, we can use it
-    $safeEmail = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-    
-    // first, trim the input of excess whitespace
-    $password = trim($password);
-    
-    
-    // sanitize both passwords 127 so
-    $safePassword = filter_input(INPUT_POST, "password",       FILTER_SANITIZE_SPECIAL_CHARS);
-    $safeConfirm = filter_input(INPUT_POST, "confirmPassword", FILTER_SANITIZE_SPECIAL_CHARS);
-    
-    // email is safe
-    echo "<p id='outputArea'>Welcome " . $userName . ". Please confirm your e-mail address to complete your registration.</p>";
+	       // first, trim the input of excess whitespace and sanitize both passwords 127 so
+	       $safePassword = trim(filter_input(INPUT_POST, "password",       FILTER_SANITIZE_STRING));
+	       $safeConfirm = trim(filter_input(INPUT_POST, "confirmPassword", FILTER_SANITIZE_STRING));
+    	       
+	       // ensure the passwords match
+	       if($safePassword !== $safeConfirm) {
+		    throw(new Exception("Please type matching passwords"));
+	       }
+	       
+	       // everything checks out
+	       echo "Welcome $userName Please confirm your e-mail address $safeEmail to complete your registration.";       
+	       
+	  }
 
-    //echo $safePassword . ", " .$safeConfirm . "</p>"; 
-    // ensure the passwords match
-    if($safePassword !== $safeConfirm) {
-        throw(new Exception("Please match passwords"));
+	  // catch the exception and format it as an error message
+	  catch (Exception $error) {
+	       echo "<span class='badForm'>" . $error->getMessage() . "</span>";
+	  }	
+        
+     }
+    else {
+        echo "<span class='badform'>CSRF verification failed.</span>";
     }
-    
-
-// catch the exception and format it as an error message
-} catch (Exception $error) {
-    echo "<span class='badForm'>" . $error->getMessage() . "</span>";
+}
+catch(RuntimeException $runtime) {
+    echo "<span class='badForm'>Unable to verify CSRF token: </span>" . $runtime->getMessage();
 }
 ?>
-<!--footer-->
-		<div class="row">
-		    <div class="text-center">
-			<section id= "footer" class= "col-xs-10" >
-			    <h2>...</h2>
-			    <p>...</p>
-			</section>
-		    </div>
-		</div>
-	    </div>
-        </div>
-    </body>
-</html>
--- register.php
+++ register.php
@@ -1,28 +1,24 @@
 <?php
 require_once("header.html");
 ?>
-	       <!--start of form-->
-	     
-				   <p>
-				   <!--<h2 class="text-center"></h2><br><br><br>-->
-					<form class="text-center col-xs-10" id="signUpForm" method="post" action="php/signup.php">
-					    <p style="color: red">*required</p><br>
-					    <label for="firstName">First Name</label><br>
-					    <input type="text" id="firstName" name="firstName" placeholder="first name(optional)" /><br><br>
-					    <label for="lastName">Last Name</label><br>
-					    <input type="text" id="lastName" name="lastName" placeholder="last name(optional)"/><br><br>
-					    <label for="userName" style="color: red">*User Name</label><br>
-					    <input type="text" id="userName" name="userName" placeholder="user name"/><br><br>       
-					    <label for="password" style="color: red">*Password</label><br>
-					    <input type="password" id="password" name="password" placeholder="password" /><br><br>
-					    <label for="confirmPassword" style="color: red">*Confirm Password</label><br>
-					    <input type="password" id="confirmPassword" name="confirmPassword" placeholder="confirm password" /><br><br>
-					    <label for="email" style="color: red">*E-mail</label><br>
-					    <input type="email" id="email" name="email" placeholder="E-mail" /><br><br>
-					    <button type="submit">Submit</button>
-					</form>
+     <form class="text-center col-xs-10" id="signUpForm" method="post" action="php/signup.php">
+	  <p style="color: red">*required</p><br>
+	  <label for="firstName">First Name</label><br>
+	  <input type="text" id="firstName" name="firstName" placeholder="first name(optional)" /><br><br>
+	  <label for="lastName">Last Name</label><br>
+	  <input type="text" id="lastName" name="lastName" placeholder="last name(optional)"/><br><br>
+	  <label for="userName" style="color: red">*User Name</label><br>
+	  <input type="text" id="userName" name="userName" placeholder="user name"/><br><br>       
+	  <label for="password" style="color: red">*Password</label><br>
+	  <input type="password" id="password" name="password" placeholder="password" /><br><br>
+	  <label for="confirmPassword" style="color: red">*Confirm Password</label><br>
+	  <input type="password" id="confirmPassword" name="confirmPassword" placeholder="confirm password" /><br><br>
+	  <label for="email" style="color: red">*E-mail</label><br>
+	  <input type="email" id="email" name="email" placeholder="E-mail" /><br><br>
+	  <button type="submit">Submit</button>
+     </form>
 			   
-	       <p id="outputArea"></p>
+<p id="outputArea"></p>
 <?php
 require_once("footer.html");
 ?>
