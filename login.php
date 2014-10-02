8e71677f5062e0a30c7fb3e40381da351c5df8b7
Rey Baros <reybaros@gmail.com>
2014-10-01 19:40:47 -0600

    https://app.asana.com/0/15833644813232/16917144320415 adding soon to be login page

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+require_once("header.html");
+?>
+	       <!--start of form-->
+	     
+				   <p>
+                                    
+			   <!--<h2 class="start here"></h2><br><br><br>-->
+					
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+			   <!--<h2 class="end here"></h2><br><br><br>-->
+	       <p id="outputArea"></p>
+<?php
+require_once("footer.html");
+?>
