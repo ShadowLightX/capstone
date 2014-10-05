529477fffec4bd0fdce2a2f2a973d16e81fbeecd
Rey Baros <reybaros@gmail.com>
2014-10-05 16:18:25 -0600

    https://app.asana.com/0/15843277689885/16917144320415 work on login link

--- login.php
+++ login.php
@@ -11,18 +11,75 @@ require_once("header.html");
 </style>
 </head>
 <body>
-<div id="login">
-<h1><strong>Welcome.</strong> Please login.</h1>
-<form action="javascript:void(0);" method="get">
-<fieldset>
-<p><input type="text" required value="Username" onBlur="if(this.value=='')this.value='Username'" onFocus="if(this.value=='Username')this.value='' "></p>
-<p><input type="password" required value="Password" onBlur="if(this.value=='')this.value='Password'" onFocus="if(this.value=='Password')this.value='' "></p>
-<p><a href="#">Forgot Password?</a></p>
-<p><input type="submit" value="Login"></p>
-</fieldset>
-</form>
+    <!-- starting modal code-->
 
-</div> <!-- end login -->
+    <head>
+
+        <meta charset="utf-8" />
+        <!--
+        <link href="css/neutrality.css" rel="stylesheet" type="text/css" />
+        <link href="css/neutrality.css" rel="stylesheet" type="text/css" /> -->
+    </head>
+        <header>
+            <h2>Net Neutrality Login</h2>
+        </header>
+ 
+        <!-- panel with buttons -->
+        <div class="main" img src="images/network_neutrality-mar.jpg">
+            <div class="panel">
+                <a href="#login_form" id="login_pop" >Log In</a>
+                <!--<a href="#join_form" id="join_pop">Sign Up</a> -->
+            </div>
+        </div>
+        <!-- popup form #1 -->
+        <a href="#x" class="overlay" id="login_form"></a>
+        <div class="popup">
+            <h2>Welcome Guest!</h2>
+            <p>Please enter your login and password here</p>
+            <div>
+                <label for="login">Login</label>
+                <input type="text" id="login" value="" />
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" value="" />
+            </div> 
+            <input type="button" value="Log In" />
+            <a class="close" href="#close"></a>
+        </div>
+        <!-- popup form #2 -
+        <a href="#x" class="overlay" id="join_form"></a>
+        <div class="popup"> -->
+            <!-- <h2>Sign Up</h2>--><!--
+            <p>Please enter your details here</p>
+            <div>
+                <label for="email">Login (Email)</label>
+                <input type="text" id="email" value="" /> 
+            </div>
+            <!--<div>
+                <label for="pass">Password</label>
+                <input type="password" id="pass" value="" />
+            </div> 
+            <div>
+                <label for="firstname">First name</label>
+                <input type="text" id="firstname" value="" />
+            </div>
+            <div>
+                <label for="lastname">Last name</label>
+                <input type="text" id="lastname" value="" />
+            </div> -->
+            <!--
+            <input type="button" <!--value="Sign Up" />&nbsp;&nbsp;&nbsp;or&nbsp;&nbsp;&nbsp;<a href="#login_form" id="login_pop">Log In</a>
+            <a class="close" href="#close"></a> 
+        </div>
+        -->
+    
+
+
+
+
+
+    <!-- eding modal code-->
 </body>
 </html>                    
                                         
