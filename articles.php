0d936e94d5bf23f3227c67a720fb6b2cd152008e
charlie goodan <cgoodan@gmail.com>
2014-10-06 10:42:39 -0600

    added articles id to header and articles to stylesheet https://app.asana.com/0/15833644813498/15833644813498

--- articles.php
+++ articles.php
@@ -2,10 +2,9 @@
 require_once("header.html");
 ?>
 	    <!--articles directory-->
-	    <ul>
+	    <ul id="articles">
 		<li class="text-center col-xs-10"><a href="article-template.html">Article</a></li>
-		<li class="text-center col-xs-10"><a href="articles-template1.html">Article1</a></li>
-		<li class="text-center col-xs-10"><a href="articles-template2.html">Article2</a></li>
+		<li class="text-center col-xs-10"><a href="article-template1.html">Article1</a></li>
 	    </ul>
 <?php
 require_once("footer.html");
