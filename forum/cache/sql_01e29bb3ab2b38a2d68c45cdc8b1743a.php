d2c19603b5b444892375580af1dd877a8c2121df
Ruben Modesto Ayala Ruth <mayala@bootcamp-coders.cnm.edu>
2014-10-07 09:35:30 -0600

    added logo and a new style, trying to see if it will work https://app.asana.com/0/15833644813536/16117410053483

--- forum/cache/sql_01e29bb3ab2b38a2d68c45cdc8b1743a.php
+++ forum/cache/sql_01e29bb3ab2b38a2d68c45cdc8b1743a.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1412637724
+1412693874
 SELECT s.style_id, c.theme_id, c.theme_data, c.theme_path, c.theme_name, c.theme_mtime, i.*, t.template_path FROM net_neutrality_styles s, net_neutrality_styles_template t, net_neutrality_styles_theme c, net_neutrality_styles_imageset i WHERE s.style_id = 3 AND t.template_id = s.template_id AND c.theme_id = s.theme_id AND i.imageset_id = s.imageset_id
 63562
 a:1:{i:0;a:11:{s:8:"style_id";s:1:"3";s:8:"theme_id";s:1:"3";s:10:"theme_data";s:63150:"/*  phpBB3 Style Sheet
--- forum/cache/sql_2d9738213f5885151805319ee2c6089d.php
+++ forum/cache/sql_2d9738213f5885151805319ee2c6089d.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1412638028
+1412694195
 SELECT forum_id, forum_name, parent_id, forum_type, left_id, right_id FROM net_neutrality_forums ORDER BY left_id ASC
 333
 a:2:{i:0;a:6:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:7:"General";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"2";}i:1;a:6:{s:8:"forum_id";s:1:"4";s:10:"forum_name";s:13:"Introductions";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:7:"left_id";s:1:"3";s:8:"right_id";s:1:"4";}}
