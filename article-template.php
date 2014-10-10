a6f3b5129db6cf231b1e6c75149e6c32ebbadab0
Matt Call <mattysc42@gmail.com>
2014-10-10 10:19:01 -0600

    https://app.asana.com/0/15833644813422/15833644813422 updated content on article template

--- article-template.php
+++ article-template.php
@@ -3,28 +3,46 @@ require_once("header.html");
 ?>
 	    <!--title-->
 	    <section id="title" class="col-xs-10 title text-center">
-		<h2>Title</h2>
+		<h1>The Comcast and Time Warner Merger</h1>
 	    </section>
 	    <!--date-->
 	    <section id="date" class="col-xs-10 col-md-3 date text-center">
-		<h2>Date</h2>
-	    </section>
-	    <!--links-->
-	    <section id="links" class="col-xs-10 col-md-4 links text-center">
-		<h2>Links</h2>
+		<h2>2014-10-10</h2>
 	    </section>
 	    <!--author-->
 	    <section id="author" class="col-xs-10 col-md-3 author text-center">
-		<h2>Author</h2>
+		<h5>Matthew Call</h5>
 	    </section>
 	    <!--content-->
 	    <section id="content" class="col-xs-10 content">
-		<img class="img-responsive" src="images/...">
-		<h2>Content</h2>
+		<p>Most people who live in the USA have heard of 'Comcast' and 'Time Warner Cable', the two largest internet service providers (ISPs) in the country.  If you live in a city, and have high speed internet hooked up in your house, there is a solid chance that you're using one of them.</p>
+    
+    <p>Comcast is the largest ISP in the States.  They are notorious for having the worst customer service in the industry, yet people still pay them for their internet service. <a href="http://www.theverge.com/2014/7/15/5901057/comcast-call-cancel-service-ryan-block">here's a link to a recent example of a Comcast customer service nightmare when a customer Ryan Block called Comcast to cancel his service</a>.  Many of you may ask why people are still willing to pay for Comcast instead of switching to a different ISP.  The largest reason is simple.  The majority of the USA only has one of two ISPs to choose from.  If you have Comcast in your area, you probably don't have much choice in who your ISP is.</p>
+    
+    <p>If you live in a region without Comcast you won't have to deal with the worst customer service in the ISP industry, but you might well have to deal with the second worst.  Time Warner Cable, while not as bad as Comcast, is a close runner-up for terrible service.  A quick google search can give thousands of complaints about Time Warner Cable's customer service.  However, like with Comcast, in most areas with Time Warner Cable there is no alternative service.</p>
+    
+    <p>Does this situation seem bad?  It could get a lot worse in the coming months.  On Feb. 13, 2014 Comcast announced that Time Warner Cable agreed to be bought out by comcast for 45 billion dollars.  This would give comcast a near monopoly of internet and cable service in a great many large USA cities.  Currently, all that's stopping the merger is an FCC (self)imposed Oct 29, 2014 deadline for the FCC to decide whether the merger should be allowed.  There is a page on the FCC website for consumers to file comments or complaints on the merger (link at the bottom of the page).</p>
+    
+    <p>So, what effects would there be on the end user?  First, comcast could charge whatever they want for their internet and people would still pay it.  If paying 90 dollars a month for internet feels like a rip-off, wait a few years and you could be paying twice that.  If the merger happens it'll mean the federal government has implicitly allowed a monopoly of internet service, and without largscale public outcry there won't be anything stopping Comcast from getting even more greedy.  In today's society internet is almost manditory for most lines of work.  If you need a job you'd best have an email address.  Even to get SSI and disability benefits, all deposits are over the internet nowadays.</p>
+    
+    <p>What are the odds of the FCC actually taking action to stop the merger?  Slimmer than companies opposing the merger, such as Netflix, would like.  Comcast has paid millions to 76 lobbyists, over 24 firms, to help increase the odds of the merger going through.  Furthermore, several of the top FCC officials, like FCC Chairman Tom Wheeler, were formerly active telecommunication lobbyists.</p>
+    
+    <p>Now, the big question is, "What can I, the end user, do about this?"
+    One method is to sign petitions online.
+    <a href ="http://apps.fcc.gov/ecfs/proceeding/view?name=14-57">You could also write comments to the FCC here</a>.  There is also the option of filing complaints to senators.  If enough Americans can publically oppose the merger, there is a chance of it not going through.</p>
+    
+    <p>For more information and news about the merger, and how it affects you, visit <br>
+    <a href="http://www.commoncause.org/press/press-releases/400000-join-chorus-of-protest-against-comcast-time-warner-cable-merger.html">Commoncause.org's protest against the merger</a> <br>
+    <a href="http://bgr.com/2014/10/03/comcast-time-warner-protests-consumer-advocates/">bgr.com's Consumer advocates turn up the heat on Comcast/TWC merger protests</a> <br>
+    <a href="http://www.freepress.net/resource/105883/join-fight-stop-comcast-time-warner-cable-merger">freepress.net's Join the Fight to Stop the Comcast-Time Warner Cable Merger</a> <br>
+    <a href="http://onpoint.wbur.org/2014/02/17/comcast-net-neutrality-fcc">onpoint.wbur.org's Big Questions In Comcast’s Time Warner Takeover Bid</a><br>
+    <a href="http://time.com/79569/comcast-has-about-76-lobbyists-working-washington-on-cable-merger-this-is-why/">time.com's Comcast Has About 76 Lobbyists Working Washington On The Time Warner Cable Merger. This is Why.</a><br>
+    <a href="http://www.washingtonpost.com/business/economy/comcast-time-warner-agree-to-merge-in-45-billion-deal/2014/02/13/7b778d60-9469-11e3-84e1-27626c5ef5fb_story.html">washingtonpost.com's Comcast, Time Warner agree to merge in $45 billion deal</a><br>
+    <a href="http://www.washingtonpost.com/business/technology/comcast-time-warner-to-merge-what-happens-to-my-service/2014/02/13/b285f81e-94b4-11e3-83b9-1f024193bb84_story.html">washingtonpost.com's Comcast, Time Warner to merge: What happens to my service?</a></p><br>
 	    </section>
 	    <!--publisher-->
 	    <section id="publisher" class="col-xs-10 publisher text-center">
-		<h2>Publisher</h2>
+		<h4>Net Neutrality For All</h4>
 	    </section>
 <?php
 require_once("footer.html");
