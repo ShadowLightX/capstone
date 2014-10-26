c0b728be74eb939ce767bd9c01fa44c5b98122dc
Modesto Ayala Ruth <tigrecientifico@gmail.com>
2014-10-26 13:42:25 -0600

    removed phpBB

--- forum/cache/data_cfg_theme_art_phantom.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1444252505
-163
-a:5:{s:4:"name";s:16:"Artodia: Phantom";s:9:"copyright";s:18:"&copy; Artodia.com";s:7:"version";s:5:"2.0.1";s:14:"parse_css_file";b:1;s:8:"filetime";i:1412716350;}
--- forum/cache/sql_6075dcc8462e84907166b0f131103696.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1412716899
-SELECT forum_id FROM net_neutrality_forums WHERE forum_options & 2 <> 0 LIMIT 1
-6
-a:0:{}
